fe0dd6144310875f130f140c7e539cd57de6e084
Nadezhda <nan.dudova@gmail.com>
2022-09-22 12:48:32 +0300

    Add files via upload

---
+++ 06 Basic CRUD Exercise/24 All Diablo Characters.sql
@@ -0,0 +1,16 @@
+/*
+24. All Diablo Characters 
+Display the name of all characters in alphabetical order. Submit your query statements as Prepare DB & run queries. 
+Example 
+name 
+Amazon 
+Assassin 
+Barbarian 
+… 
+*/
+
+SELECT 
+    `name`
+FROM
+    `characters`
+ORDER BY `name`;
