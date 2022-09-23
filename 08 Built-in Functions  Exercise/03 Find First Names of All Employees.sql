831e0ca09e3effeaed010967ff4fe10e4ccd7958
Nadezhda <nan.dudova@gmail.com>
2022-09-23 09:58:27 +0300

    Add files via upload

---
+++ 08 Built-in Functions  Exercise/03 Find First Names of All Employees.sql
@@ -0,0 +1,19 @@
+/*
+3.	Find First Names of All Employees
+Write a SQL query to find the first names of all employees in the departments with ID 3 or 10 and whose hire year is between 1995 and 2005 inclusively. Order the information by id. Submit your query statements as Prepare DB & run queries.
+Example
+first_name
+Deborah
+Wendey
+Candy
+…
+*/
+
+SELECT 
+    `first_name`
+FROM
+    `employees`
+WHERE
+    `department_id` IN (3 , 10)
+        AND YEAR(`hire_date`) BETWEEN 1995 AND 2005
+ORDER BY `employee_id`;
