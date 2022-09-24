6944d06251574ca35e289c37ffe0282ec0430318
Nadezhda <nan.dudova@gmail.com>
2022-09-24 18:04:51 +0300

    Add files via upload

---
+++ 08 Built-in Functions  Exercise/11  Mix of Peak and River Names.sql
@@ -0,0 +1,11 @@
+/*
+11.	 Mix of Peak and River Names
+Combine all peak names with all river names, so that the last letter of each peak name is the same as the first letter of its corresponding river name. Display the peak name, the river name, and the obtained mix(converted to lower case). Sort the results by the obtained mix alphabetically. Submit your query statements as Prepare DB & run queries.
+
+Example
+peak_name	river_name	mix
+Aconcagua	Amazon	aconcaguamazon
+Aconcagua	Amur	aconcaguamur
+Banski Suhodol	Lena	banski suhodolena
+…	…	…
+*/
