7494e2e55d07de75dece58231a709de3fe977987
Nadezhda <nan.dudova@gmail.com>
2022-09-24 18:03:36 +0300

    Add files via upload

---
+++ 08 Built-in Functions  Exercise/07 Find Towns Not Starting With.sql
@@ -0,0 +1,10 @@
+/*
+7.	 Find Towns Not Starting With
+Write a SQL query to find all towns that do not start with letters R, B or D (case insensitively). Order them alphabetically by name. Submit your query statements as Prepare DB & run queries.
+Example
+town_id	name
+2	Calgary
+23	Cambridge
+15	Carnation
+…	…
+*/
