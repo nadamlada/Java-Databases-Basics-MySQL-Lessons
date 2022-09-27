63363b5db4b7689051722fb29011f17fc5930912
Nadezhda <nan.dudova@gmail.com>
2022-09-27 13:19:37 +0300

    Add files via upload

---
+++ 09 Data Aggregation/04 Appetizers Count.sql
@@ -0,0 +1,11 @@
+/*
+4.	 Appetizers Count
+Write a query to retrieve the count of all appetizers (category id = 2) with price higher than 8. Submit your queries with the MySQL prepare DB & run queries strategy. 
+*/
+
+SELECT 
+    COUNT(*)
+FROM
+    `products`
+WHERE
+    `category_id` = 2 AND `price` > 8;
