542a05d61521cbc55c0168263d48f4d8a7508f3a
Nadezhda <nan.dudova@gmail.com>
2022-09-28 07:51:32 +0300

    Add files via upload

---
+++ 10 Data Aggregation Exercise/08 Deposit Charge.sql
@@ -0,0 +1,23 @@
+/*
+8. Deposit Charge
+Create a query that selects:
+•	Deposit group 
+•	Magic wand creator
+•	Minimum deposit charge for each group 
+Group by deposit_group and magic_wand_creator.
+Select the data in ascending order by magic_wand_creator and deposit_group.
+
+Example:
+deposit_group	magic_wand_creator	min_deposit_charge
+Blue Phoenix	Antioch Peverell	30.00
+…	…	
+*/
+
+SELECT 
+    `deposit_group`,
+    `magic_wand_creator`,
+    MIN(`deposit_charge`) AS 'min_deposit_charge'
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group` , `magic_wand_creator`
+ORDER BY `magic_wand_creator` , `deposit_group`;
