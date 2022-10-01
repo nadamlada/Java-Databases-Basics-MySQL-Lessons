dcc5edacdcf47d65cd137e891b2b940241bc6874
Nadezhda <nan.dudova@gmail.com>
2022-10-01 16:31:22 +0300

    Add files via upload

---
+++ 12 Table Relations Exercise/06 University Database.sql
@@ -0,0 +1,47 @@
+/*
+6.	University Database
+Create a new database and design the following structure: 
+
+Submit your queries by using "MySQL run queries & check DB" strategy.
+*/
+
+-- CREATE DATABASE `uni`;
+-- USE `uni`;
+
+CREATE TABLE `majors` (
+    `major_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `students` (
+    `student_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `student_number` VARCHAR(12),
+    `student_name` VARCHAR(50),
+    `major_id` INT,
+    CONSTRAINT `fk_students_majors` FOREIGN KEY (`major_id`)
+        REFERENCES `majors` (`major_id`)
+);
+
+CREATE TABLE `payments` (
+    `payment_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `payment_date` DATE,
+    `payment_amount` DECIMAL(8 , 2 ),
+    `student_id` INT,
+    CONSTRAINT `fk_payments_students` FOREIGN KEY (`student_id`)
+        REFERENCES `students` (student_id)
+); 
+
+CREATE TABLE `subjects` (
+    `subject_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `subject_name` VARCHAR(50)
+);
+
+CREATE TABLE `agenda` (
+    `student_id` INT NOT NULL,
+    `subject_id` INT NOT NULL,
+    CONSTRAINT `pk_agenda` PRIMARY KEY (`student_id` , `subject_id`),
+    CONSTRAINT `fk_agenda_subjects` FOREIGN KEY (`subject_id`)
+		REFERENCES `subjects` (`subject_id`),
+	CONSTRAINT `fk_agenda_student_id` FOREIGN KEY (`student_id`)
+		REFERENCES `students` (`student_id`)
+);
