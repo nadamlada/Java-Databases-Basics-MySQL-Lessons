ea381cbeaf3ca13e778a8152be5a000444c9b20b
Nadezhda <nan.dudova@gmail.com>
2022-10-01 16:32:07 +0300

    Delete 07 SoftUni Design.sql

--- 12 Table Relations Exercise/07 SoftUni Design.sql
+++
@@ -1,4 +0,0 @@
-/*
-7.	SoftUni Design
-Create an E/R Diagram of the SoftUni Database. There are some special relations you should check out: employees are self-referenced (manager_id) and departments have One-to-One with the employees (manager_id) while the employees have One-to-Many (department_id). You might find it interesting how it looks on а diagram. 
-*/
