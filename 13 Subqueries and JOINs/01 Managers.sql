dc8228a451982b47fab84fb733138784931d4cbc
Nadezhda <nan.dudova@gmail.com>
2022-10-02 16:17:27 +0300

    Add files via upload

---
+++ 13 Subqueries and JOINs/01 Managers.sql
@@ -0,0 +1,10 @@
+/*
+1.	Managers
+Write a query to retrieve information about the managers – id, full_name, deparment_id and department_name. Select the first 5 departments ordered by employee_id. Submit your queries using the "MySQL prepare DB and Run Queries" strategy. 
+
+Example
+employee_id	full_name	department_id	department_name
+3	Roberto Tamburello	10	Finance
+4	Rob Walters	2	Tool Design
+…	…	…	…
+*/
