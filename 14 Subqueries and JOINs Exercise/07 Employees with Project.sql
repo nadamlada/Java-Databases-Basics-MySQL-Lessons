1cf47d71c378a432918d6ca6a6b95c7b6728c654
Nadezhda <nan.dudova@gmail.com>
2022-10-04 19:42:06 +0300

    Add files via upload

---
+++ 14 Subqueries and JOINs Exercise/07 Employees with Project.sql
@@ -0,0 +1,30 @@
+/*
+7.	Employees with Project
+Write a query that selects:
+•	employee_id
+•	first_name
+•	project_name
+Filter only employees with a project, which has started after 13.08.2002 and it is still ongoing (no end date). Return the first 5 rows sorted by first_name then by project_name both in ascending order.
+
+Example
+employee_id	first_name	project_name
+44	A. Scott	Hitch Rack - 4-Bike
+170	Alan	LL Touring Handlebars
+…	…	…
+*/
+
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `p`.`name` AS 'project_name'
+FROM
+    `employees` AS `e`
+        JOIN
+    `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+        JOIN
+    `projects` AS `p` ON `p`.`project_id` = `ep`.`project_id`
+WHERE
+    DATE(`p`.`start_date`) > '2002-08-13'
+        AND `p`.`end_date` IS NULL
+ORDER BY `e`.`first_name` , `p`.`name`
+LIMIT 5;
