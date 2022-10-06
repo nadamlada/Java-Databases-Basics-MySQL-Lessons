47140a2a25eca44334df3aa341b1ea25f7ea3e28
Nadezhda <nan.dudova@gmail.com>
2022-10-06 18:10:31 +0300

    Add files via upload

---
+++ 14 Subqueries and JOINs Exercise/17 Highest Peak and Longest River by Country.sql
@@ -0,0 +1,30 @@
+/*
+17.  Highest Peak and Longest River by Country
+For each country, find the elevation of the highest peak and the length of the longest river, sorted by the highest peak_elevation (from highest to lowest), then by the longest river_length (from longest to smallest), then by country_name (alphabetically). Display NULL when no data is available in some of the columns. Limit only the first 5 rows.
+
+Example
+country_name	highest_peak_elevation	longest_river_length
+China	8848	6300
+India	8848	3180
+Nepal	8848	2948
+Pakistan	8611	3180
+Argentina	6962	4880
+*/
+
+SELECT 
+    `c`.`country_name`,
+    MAX(`p`.`elevation`) AS 'highest_peak_elevation',
+    MAX(`r`.`length`) AS 'longest_river_length'
+FROM
+    `countries` AS `c`
+        LEFT JOIN
+    `mountains_countries` AS `mc` ON `mc`.`country_code` = `c`.`country_code`
+        LEFT JOIN
+    `peaks` AS `p` ON `p`.`mountain_id` = `mc`.`mountain_id`
+        LEFT JOIN
+    `countries_rivers` AS `cr` ON `cr`.`country_code` = `c`.`country_code`
+        LEFT JOIN
+    `rivers` AS `r` ON `r`.`id` = `cr`.`river_id`
+GROUP BY `c`.`country_name`
+ORDER BY `highest_peak_elevation` DESC , `longest_river_length` DESC , `c`.`country_name`
+LIMIT 5;
