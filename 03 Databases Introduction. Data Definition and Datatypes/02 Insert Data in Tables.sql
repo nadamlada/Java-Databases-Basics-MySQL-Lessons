746db6e664d3c3c12a53319ac3574ffc7d64c151
Nadezhda <nan.dudova@gmail.com>
2022-10-10 09:05:22 +0300

    Add files via upload

---
+++ 03 Databases Introduction. Data Definition and Datatypes/02 Insert Data in Tables.sql
@@ -0,0 +1,9 @@
+/*
+2. Insert Data in Tables
+Inserting data can be done with a query too. To do that we use the "INSERT" clause. Populate the "employees" table with 3 test values.
+*/
+
+INSERT INTO `employees`(`first_name`, `last_name`) VALUES ("Pet", "Petrov");
+INSERT INTO `employees`(`first_name`, `last_name`) VALUES 
+("Ger", "Gerov"),
+("Ivan", "Ivanov");
