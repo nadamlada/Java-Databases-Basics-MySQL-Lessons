27348b1bd6ee144edcc425bfb5baf57b0ea35843
Nadezhda <nan.dudova@gmail.com>
2022-10-10 09:05:50 +0300

    Add files via upload

---
+++ 03 Databases Introduction. Data Definition and Datatypes/04 Adding Constraints.sql
@@ -0,0 +1,8 @@
+/*
+4. Adding Constraints
+Create the connection via foreign key between the "products" and "categories" tables that you've created earlier. Make "category_id" foreign key linked to "id" in the "categories" table. 
+*/
+ALTER TABLE `products`
+ADD CONSTRAINT `index_category_id`
+FOREIGN KEY(`category_id`)
+REFERENCES `categories`(`id`);
