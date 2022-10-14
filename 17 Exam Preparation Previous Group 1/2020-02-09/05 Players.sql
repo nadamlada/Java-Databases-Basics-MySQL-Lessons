d32413b84eaced3d88ea5654f86390887a52bc2e
Nadezhda <nan.dudova@gmail.com>
2022-10-14 20:52:59 +0300

    Add files via upload

---
+++ 17 Exam Preparation Previous Group 1/2020-02-09/05 Players.sql
@@ -0,0 +1,16 @@
+/*
+5.	Players 
+Extract from the Football Scout Database (fsd) database, info about all of the players. 
+Order the results by players - salary descending.
+Required Columns
+•	first_name
+•	age
+•	salary
+
+Example
+first_name		age	salary
+Renault	24	984113.71
+Akim	31	982188.88
+Mollie	24	966079.07
+…	…	…
+*/
