0d417a13bc4c3acd9559d66c8181d4c5fa501bfe
Nadezhda <nan.dudova@gmail.com>
2022-10-16 18:03:23 +0300

    Add files via upload

---
+++ 18 Exam Preparation Previous Group 2/2020-03-31/02 Insert.sql
@@ -0,0 +1,25 @@
+/*
+02.	Insert
+You will have to insert records of data into the addresses table, based on the users table. 
+For users with male gender, insert data in the addresses table with the following values:
+•	address – set it to username of the user.
+•	town – set it to password of the user.
+•	country – set it to ip of the user. 
+•	user_id – set it to age of the user. 
+*/
+
+INSERT INTO `addresses` (
+	`address`,
+    `town`,
+    `country`,
+    `user_id`
+)
+
+SELECT
+	`u`.`username`,
+    `u`.`password`,
+    `u`.`ip`,
+    `u`.`age`
+FROM
+	`users` AS `u`
+WHERE `u`.`gender` = 'M';
