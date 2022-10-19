4711c401fb62931a3a91a22c0c0b91e8cd06b6ef
Nadezhda <nan.dudova@gmail.com>
2022-10-19 19:43:43 +0300

    Add files via upload

---
+++ 18 Exam Preparation Previous Group 2/2020-03-31/07_1 Lucky Users.sql
@@ -0,0 +1,23 @@
+/*
+07.	Lucky Users
+When the user has the same id as its photo, it is considered Lucky User. Extract from the database all lucky users. 
+Extract id_username (concat id + " " + username) and email of all lucky users. Order the results ascending by user id.
+Required Columns
+•	id_username
+•	email
+
+Example
+id_username	email
+12 aroccob	dpendrichb@hhs.gov
+…	…
+*/
+
+SELECT 
+    CONCAT(`u`.`id`, ' ', `u`.`username`) AS 'id_username',
+    `u`.`email`
+FROM
+    `users` AS `u`
+        LEFT JOIN
+    `users_photos` AS `up` ON `up`.`user_id` = `u`.`id`
+        AND `up`.`user_id` = `up`.`photo_id`
+ORDER BY `up`.`user_id`;
