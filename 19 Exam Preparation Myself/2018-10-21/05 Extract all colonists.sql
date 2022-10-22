f146645c98fe6e6d024bb55e7218f1c8289d78b1
Nadezhda <nan.dudova@gmail.com>
2022-10-22 15:31:47 +0300

    Add files via upload

---
+++ 19 Exam Preparation Myself/2018-10-21/05 Extract all colonists.sql
@@ -0,0 +1,23 @@
+/*
+05 Extract all colonists
+Extract from the database, all colonists. Sort the results by first name, them by last name, and finally by id in ascending order.
+
+Required Columns
+•	id
+•	full_name(first_name + last_name separated by a single space)
+•	ucn
+
+Example
+id	full_name	ucn
+35	Aigneis McConville	9225403496
+92	Althea Kelinge	9998159318
+...	...	...
+*/
+
+SELECT 
+    `id`,
+    CONCAT(`first_name`, ' ', `last_name`) AS 'full_name',
+    `ucn`
+FROM
+    `colonists`
+ORDER BY `first_name` , `last_name` , `id`
